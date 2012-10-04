38cbea0281bf674da3892f337cdfb28d7e2aa60e
Miro Hrončok <miro@hroncok.cz>
2012-10-04 12:29:58 +0200

    Debug

--- php/js.js
+++ php/js.js
@@ -45,13 +45,14 @@ function dwnLink(job) {
 			if (textfile.readyState==4 && textfile.status == 200){
 				var linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
 				clearInterval(dwnInterval);
+				clearInterval(refreshInterval);
 			} else {
 				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
 			}
 			if (linkHTML!="") {
 				document.getElementById("topDwnLink").innerHTML=linkHTML;
 				document.getElementById("bottomDwnLink").innerHTML=linkHTML;
-				clearInterval(refreshInterval);
+				
 			}
 		}
 		textfile.open("GET","files/"+job+"_export.gcode",true);
