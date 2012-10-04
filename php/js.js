a30e5998c32bde2f6a4a31bbc1816b299fa433ba
Miro Hrončok <miro@hroncok.cz>
2012-10-04 10:34:28 +0200

    JS debuging

--- php/js.js
+++ php/js.js
@@ -42,7 +42,7 @@ function dwnLink(job) {
 		textfile=new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	textfile.onreadystatechange=function() {
-		if (textfile.readyState==4 && checkEnd(job)) {
+		if (textfile.readyState==4 && checkEnd(job)==1) {
 			if (textfile.status == 404) {
 				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
 			}
