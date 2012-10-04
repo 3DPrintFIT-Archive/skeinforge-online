d211fbdf3c77c06822224d18fa346d77cb942874
Miro Hrončok <miro@hroncok.cz>
2012-10-04 05:14:41 +0200

    Download link commented

--- php/index.php
+++ php/index.php
@@ -44,6 +44,21 @@ function putForm() {
 		xmlhttp.open("GET","files/"+job+".log",true);
 		xmlhttp.send();
 	}
+	function dwnLink(job) {
+		var xmlhttp;
+		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		} else {// code for IE6, IE5
+			xmlhttp2=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.open("GET","files/"+job+"_export.gcode",true);
+		xmlhttp.send();
+		if (xmlhttp.status == 200){
+			var linkHTML = "<p><strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.</p>\n");
+			document.getElementById("topDwnLink").innerHTML=linkHTML;
+			document.getElementById("bottomDwnLink").innerHTML=linkHTML;
+		}
+	}
 	</script>
 </head>
 <body>
