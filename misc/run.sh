95f28d9a498d51371d7860c76fd10cdc97978292
Miro Hrončok <miro@hroncok.cz>
2012-10-04 04:48:43 +0200

    do not handle the end

--- misc/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python ../libs/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3" 2>&1
-echo end
--- php/index.php
+++ php/index.php
@@ -89,7 +89,8 @@ function dwnLink($basename) {
 			$fullpath = 'files/';
 			exec('cp -ar ../prefdir '.$fullpath.$basename.'.dir');
 			exec('echo -e "Profile Selection:\t'.$profile.'" >> '.$fullpath.$basename.'.dir/profiles/extrusion.csv');
-			exec('../misc/run.sh '.$fullpath.$basename.'.dir '.$fullpath.$filename.' '.$fullpath.$basename.'.log > '.$fullpath.$basename.'.exit 2>&1 &',$output,$exitcode);
+			$skeinforge = 'python ../libs/skeinforge_application/skeinforge.py -p '.$fullpath.$basename.'.dir';
+			exec($skeinforge.' '.$fullpath.$filename.' > '.$fullpath.$basename.'.log 2>&1 &',$output,$exitcode);
 		}
 	} elseif($_GET["job"] != "") {
 		if(file_exists("files/".$_GET["job"])) {
