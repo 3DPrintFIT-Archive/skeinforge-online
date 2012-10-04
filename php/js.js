81fe5cbfa068f91ac26bced6820d11d0708bd22e
Miro Hrončok <miro@hroncok.cz>
2012-10-04 10:09:59 +0200

    check if it is 404

--- php/js.js
+++ php/js.js
@@ -22,14 +22,16 @@ function dwnLink(job) {
 		textfile=new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	textfile.onreadystatechange=function() {
-		if (textfile.readyState==4 && textfile.status == 200) {
-			var linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
-			document.getElementById("topDwnLink").innerHTML=linkHTML;
-			document.getElementById("bottomDwnLink").innerHTML=linkHTML;
-			clearInterval(dwnInterval);
-			clearInterval(refreshInterval);
-		} else {
-			setTimeout(function(){checkEnd(job)},3000);
+		if (textfile.readyState==4) {
+			if (textfile.status == 200) {
+				var linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
+				document.getElementById("topDwnLink").innerHTML=linkHTML;
+				document.getElementById("bottomDwnLink").innerHTML=linkHTML;
+				clearInterval(dwnInterval);
+				clearInterval(refreshInterval);
+			} else if (textfile.status == 404) {
+				setTimeout(function(){checkEnd(job)},3000);
+			}
 		}
 	}
 	textfile.open("GET","files/"+job+"_export.gcode",true);
