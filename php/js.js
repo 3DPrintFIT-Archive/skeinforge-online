22d8b8ef979956eb2af5a0fe6da05f3e318edf98
Miro Hrončok <miro@hroncok.cz>
2012-10-04 12:11:33 +0200

    Debug

--- php/js.js
+++ php/js.js
@@ -15,75 +15,46 @@ function loadLog(job,ext) {
 }
 
 function checkEnd(job) {
-	document.getElementById("infobox").innerHTML="Checking end of the job";
+	var willreturn = 0;
 	var textfile;
-	var retvalue = 0;
 	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
 		textfile=new XMLHttpRequest();
 	} else {// code for IE6, IE5
 		textfile=new ActiveXObject("Microsoft.XMLHTTP");
 	}
+	textfile.onreadystatechange=function() {
+		if (textfile.readyState==4 && textfile.status==200 && textfile.responseText[0]=="e"){
+			willreturn = 1;
+		}
+	}
 	textfile.open("GET","files/"+job+".exit",true);
 	textfile.send();
-	if (textfile.readyState==4 && textfile.status == 200) {
-		document.getElementById("infobox").innerHTML="The file exists";
-		if (textfile.responseText[0] == "e") {
-			document.getElementById("infobox").innerHTML="The file contains end mark";
-			retvalue = 1;
-		} else {
-			document.getElementById("infobox").innerHTML="Doesn't contain end mark";
-		}
-	} else {
-		document.getElementById("infobox").innerHTML="File files/"+job+".exit not present/ready";
-	}
-	return retvalue;
+	return willreturn;
 }
 
-/*function dwnLink(job) {
-	if (checkEnd(job)==0) {
+
+function dwnLink(job) {
+	if (checkEnd(job)==1) {
 		var textfile;
-		var linkHTML="";
 		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
 			textfile=new XMLHttpRequest();
 		} else {// code for IE6, IE5
 			textfile=new ActiveXObject("Microsoft.XMLHTTP");
 		}
-		textfile.open("HEAD","files/"+job+"_export.gcode",true);
+		textfile.onreadystatechange=function() {
+			if (textfile.readyState==4 && textfile.status == 200){
+				var linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
+			} else {
+				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
+			}
+			if (linkHTML!="") {
+				document.getElementById("topDwnLink").innerHTML=linkHTML;
+				document.getElementById("bottomDwnLink").innerHTML=linkHTML;
+				//clearInterval(dwnInterval);
+				//clearInterval(refreshInterval);
+			}
+		}
+		textfile.open("GET","files/"+job+"_export.gcode",true);
 		textfile.send();
-		if (textfile.readyState==4 && textfile.status == 200) {
-			linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
-		} else {
-			linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
-		}
-		if (linkHTML!="") {
-			document.getElementById("topDwnLink").innerHTML=linkHTML;
-			document.getElementById("bottomDwnLink").innerHTML=linkHTML;
-			//clearInterval(dwnInterval);
-			//clearInterval(refreshInterval);
-		}
 	}
-}*/
-
-function dwnLink(job) {
-	var xmlhttp;
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	} else {// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status == 200){
-			var linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
-		} else {
-			linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
-		}
-		if (linkHTML!="") {
-			document.getElementById("topDwnLink").innerHTML=linkHTML;
-			document.getElementById("bottomDwnLink").innerHTML=linkHTML;
-			//clearInterval(dwnInterval);
-			//clearInterval(refreshInterval);
-		}
-	}
-	xmlhttp.open("GET","files/"+job+"_export.gcode",true);
-	xmlhttp.send();
 }
