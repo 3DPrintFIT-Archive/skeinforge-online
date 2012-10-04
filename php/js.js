1d78910723ee347d6d91f9efefd9a2a9c2695e02
Miro Hrončok <miro@hroncok.cz>
2012-10-04 10:24:45 +0200

    JS debuging

--- php/js.js
+++ php/js.js
@@ -43,11 +43,11 @@ function dwnLink(job) {
 		textfile=new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	textfile.onreadystatechange=function() {
-		if (textfile.readyState==4 && checkEnd(job)==1) {
+		if (checkEnd(job)==1) {
 			if (textfile.status == 404) {
 				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
 			}
-			if (textfile.status == 200) {
+			if (textfile.readyState==4 && textfile.status == 200) {
 				linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
 			}
 			//if ((textfile.status == 200) || (textfile.status == 404)) {
