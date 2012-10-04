97211dff5a40f562bb5fa98a9e70e3bb89f786d0
Miro Hrončok <miro@hroncok.cz>
2012-10-04 10:15:54 +0200

    check if it is 404

--- php/js.js
+++ php/js.js
@@ -44,10 +44,11 @@ function dwnLink(job) {
 	}
 	textfile.onreadystatechange=function() {
 		if (textfile.readyState==4 && checkEnd(job)==1) {
+			if (textfile.status == 404) {
+				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
+			}
 			if (textfile.status == 200) {
 				linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
-			} else if (textfile.status == 404) {
-				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
 			}
 			document.getElementById("topDwnLink").innerHTML=linkHTML;
 			document.getElementById("bottomDwnLink").innerHTML=linkHTML;
