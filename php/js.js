7dc5403234f1f8cf03d7e80f8b1fe45c03312c44
Miro Hrončok <miro@hroncok.cz>
2012-10-04 10:35:51 +0200

    JS debuging

--- php/js.js
+++ php/js.js
@@ -34,29 +34,32 @@ function checkEnd(job) {
 }
 
 function dwnLink(job) {
-	var textfile;
-	var linkHTML;
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		textfile=new XMLHttpRequest();
-	} else {// code for IE6, IE5
-		textfile=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	textfile.onreadystatechange=function() {
-		if (textfile.readyState==4 && checkEnd(job)==1) {
-			if (textfile.status == 404) {
-				linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
-			}
-			if (textfile.status == 200) {
-				linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
-			}
-			if ((textfile.status == 200) || (textfile.status == 404)) {
-				document.getElementById("topDwnLink").innerHTML=linkHTML;
-				document.getElementById("bottomDwnLink").innerHTML=linkHTML;
-				clearInterval(dwnInterval);
-				clearInterval(refreshInterval);
+	if (checkEnd(job)==1) {
+		var textfile;
+		var linkHTML;
+		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+			textfile=new XMLHttpRequest();
+		} else {// code for IE6, IE5
+			textfile=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		textfile.onreadystatechange=function() {
+			if (textfile.readyState==4) {
+				if (textfile.status == 404) {
+					linkHTML = "<strong>Error:</strong> The procces ended without gcode, see the log";
+				}
+				if (textfile.status == 200) {
+					linkHTML = "<strong>Download:</strong> <a href=\"files/"+job+"_export.gcode\">"+job+"_export.gcode</a> &mdash; Your files will be deleted in 24 hours.";
+				}
+				if ((textfile.status == 200) || (textfile.status == 404)) {
+					document.getElementById("topDwnLink").innerHTML=linkHTML;
+					document.getElementById("bottomDwnLink").innerHTML=linkHTML;
+					clearInterval(dwnInterval);
+					clearInterval(refreshInterval);
+				}
 			}
 		}
+		textfile.open("GET","files/"+job+"_export.gcode",true);
+		textfile.send();
 	}
-	textfile.open("GET","files/"+job+"_export.gcode",true);
-	textfile.send();
 }
+
