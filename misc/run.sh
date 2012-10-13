66c9a79858b3a9b82b07b622c8065c9112ef9cfb
Miro Hrončok <miro@hroncok.cz>
2012-10-13 23:20:13 +0200

    Use pypy and link the lib

--- misc/run.sh
+++ misc/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#../libs/pypy/bin/pypy
-python ../libs/skeinforge/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3"; echo $! > "$4"
+export LD_LIBRARY_PATH="../libs/:$LD_LIBRARY_PATH"
+../libs/pypy/bin/pypy ../libs/skeinforge/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3"; echo $! > "$4"
 echo end
