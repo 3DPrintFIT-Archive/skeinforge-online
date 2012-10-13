d20aa22a100dfefbdf266469d1cbeaeef15ac339
Miro Hrončok <miro@hroncok.cz>
2012-10-13 22:33:29 +0200

    New skeinforge path

--- misc/run.sh
+++ misc/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python ../libs/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3"; echo $! > "$4"
+#../libs/pypy/bin/pypy
+python ../libs/skeinforge/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3"; echo $! > "$4"
 echo end
