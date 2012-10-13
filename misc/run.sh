9e0c06dfbe24c468264b10f48ce90d01842d000e
Miro Hrončok <miro@hroncok.cz>
2012-10-13 23:11:04 +0200

    Do not use pypy

--- misc/run.sh
+++ misc/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-../libs/pypy/bin/pypy ../libs/skeinforge/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3"; echo $! > "$4"
+#../libs/pypy/bin/pypy
+python ../libs/skeinforge/skeinforge_application/skeinforge.py -p "$1" "$2" > "$3"; echo $! > "$4"
 echo end
